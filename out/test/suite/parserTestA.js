8da713939042014a71f089bf7dde343896c29b7c
Kuma961 <all96p@libero.it>
2023-01-13 10:45:47 +0100

    Aggiunta file di test A, F, G e S per ogni membro del team che effettuerà dei test.

---
+++ out/test/suite/parserTestA.js
@@ -0,0 +1,23 @@
+"use strict";
+//I test si eseguono tramite il comando npm run tests
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.dummy = void 0;
+//import { ... } from '../../src/parsing'; //Funzioni da testare
+const chai_1 = require("chai");
+require("mocha");
+//Test
+describe('Dummy test A', () => {
+    //Descrizione del risultato del test
+    it('should return true', () => {
+        //Corpo del test
+        const result = dummy(); //Risultato della funzione da testare
+        const expected_result = true; //Risultato atteso
+        (0, chai_1.expect)(result).to.equal(expected_result); //Asserzione
+    });
+});
+function dummy() {
+    return true;
+}
+exports.dummy = dummy;
+//Testing tokenize and tokenize_head_tail.
+//# sourceMappingURL=parserTestA.js.map
---
+++ test/suite/parserTestA.ts
@@ -0,0 +1,23 @@
+//I test si eseguono tramite il comando npm run tests
+
+//import { ... } from '../../src/parsing'; //Funzioni da testare
+import { expect } from 'chai';
+import 'mocha';
+
+//Test
+describe('Dummy test A', 
+  () => { 
+	//Descrizione del risultato del test
+    it('should return true', () => {
+		//Corpo del test
+      const result = dummy(); //Risultato della funzione da testare
+      const expected_result = true; //Risultato atteso
+      expect(result).to.equal(expected_result); //Asserzione
+  }); 
+});
+
+export function dummy(){
+    return true;
+}
+
+//Testing tokenize and tokenize_head_tail.
