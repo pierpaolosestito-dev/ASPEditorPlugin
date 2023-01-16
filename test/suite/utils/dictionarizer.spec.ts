9fbe6cb74bdcb1c14580ec92793e929f545fac9a
Pierpaolo Sestito <pierpaolo.sestito.1999@gmail.com>
2023-01-16 12:47:12 +0100

    refactoring test_dictionarizer

--- test/suite/utils/dictionarizer.spec.ts
+++ test/suite/utils/dictionarizer.spec.ts
@@ -3,7 +3,7 @@ import * as assert from 'assert';
 
 describe('Dictionarizer Test Suite', () => {
 	it('dictionarizer must construct the correct data structure', () => {
-		assert.deepStrictEqual(dictionarizer("constants.json"), 
+		assert.deepStrictEqual(dictionarizer("assets/constants.json"), 
 		{
 			"language-constants": ["U_INT", "UT_INT", "UR_INT", "T_INT", "R_INT", "CONST", "Q_CONST"]
 		});
