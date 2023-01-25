307e3b1a178eff4354ebac038ff56e935fd10b29
gioiannu <gioiannu97@libero.it>
2023-01-25 23:31:49 +0100

    Update prompter.test

--- test/suite/prompter/prompter.test.ts
+++ test/suite/prompter/prompter.test.ts
@@ -5,12 +5,18 @@ suite('Prompter Test Suite',
   () => {
     //Descrizione del risultato del test
     test('isAtStartOfBuiltins ', async () => {
+		const context = await vscode.commands.executeCommand("getContext") as vscode.ExtensionContext;
 
 		await vscode.workspace.openTextDocument({
 			language: 'text',
 			content: 'ciao'
-		}).then(doc => {
-				const prompter = new Prompter(vscode.Conte);
+		}).then(doc => {				
+				const prompter = new Prompter(context);
+				const pos :vscode.Position = {
+					line: 0, character: 0,
+				
+				};
+				prompter.isAtStartOfAggregate(doc,doc.getWordRangeAtPosition(pos)!);
 				assert.equal(doc.getText(),"ciao");
 			});
     });
