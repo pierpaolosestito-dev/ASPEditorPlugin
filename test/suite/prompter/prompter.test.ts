d54bee3e375889a8fa963eccf6f1d2076903111e
Pierpaolo Sestito <pierpaolo.sestito.1999@gmail.com>
2023-01-26 00:15:26 +0100

    mock vscode.document, now prompter test works

--- test/suite/prompter/prompter.test.ts
+++ test/suite/prompter/prompter.test.ts
@@ -1,23 +1,51 @@
-/*import { Prompter } from "../../../src/prompter";
+import { Prompter } from "../../../src/prompter";
 import * as vscode from 'vscode';
 import * as assert from 'assert';
 suite('Prompter Test Suite',
   () => {
     //Descrizione del risultato del test
-    test('isAtStartOfBuiltins ', async () => {
-		const context = await vscode.commands.executeCommand("getContext") as vscode.ExtensionContext;
-
+    test('isAtStartOfAggregates return True if triggerCharacter is #', async () => {
+		const ext = vscode.extensions.getExtension("VSCodeEditor.asp-language-support-dlv2");
+		const myExtensionContext = await ext?.activate();
 		await vscode.workspace.openTextDocument({
-			language: 'text',
-			content: 'ciao'
+			language: 'asp',
+			content: '#'
 		}).then(doc => {				
-				const prompter = new Prompter(context);
-				const pos :vscode.Position = {
-					line: 0, character: 0,
-				
-				};
-				prompter.isAtStartOfAggregate(doc,doc.getWordRangeAtPosition(pos)!);
-				assert.equal(doc.getText(),"ciao");
+				const prompter = new Prompter(myExtensionContext);
+				assert.equal(prompter.isAtStartOfAggregate(doc,new vscode.Range(new vscode.Position(0,0),new vscode.Position(0,0))),true);
 			});
     });
-  });*/
+	test('isAtStartOfBuiltins return True if triggerCharacter is &', async () => {
+		const ext = vscode.extensions.getExtension("VSCodeEditor.asp-language-support-dlv2");
+		const myExtensionContext = await ext?.activate();
+		await vscode.workspace.openTextDocument({
+			language: 'asp',
+			content: '&'
+		}).then(doc => {				
+				const prompter = new Prompter(myExtensionContext);
+				assert.equal(prompter.isAtStartOfBuiltins(doc,new vscode.Range(new vscode.Position(0,0),new vscode.Position(0,0))),true);
+			});
+    });
+	test('isAtStartOfDynamicPredicates return the triggerCharacter if triggerCharacter is a letter', async () => {
+		const ext = vscode.extensions.getExtension("VSCodeEditor.asp-language-support-dlv2");
+		const myExtensionContext = await ext?.activate();
+		await vscode.workspace.openTextDocument({
+			language: 'asp',
+			content: 'A'
+		}).then(doc => {				
+				const prompter = new Prompter(myExtensionContext);
+				assert.equal(prompter.isAtStartOfDynamicPredicates(doc,new vscode.Range(new vscode.Position(0,0),new vscode.Position(0,0))),'A');
+			});
+    });
+	test('isAtStartOfConstants return triggerCharacter if triggerCharacter is a letter or _', async () => {
+		const ext = vscode.extensions.getExtension("VSCodeEditor.asp-language-support-dlv2");
+		const myExtensionContext = await ext?.activate();
+		await vscode.workspace.openTextDocument({
+			language: 'asp',
+			content: '_'
+		}).then(doc => {				
+				const prompter = new Prompter(myExtensionContext);
+				assert.equal(prompter.isAtStartOfConstants(doc,new vscode.Range(new vscode.Position(0,0),new vscode.Position(0,0))),'_');
+			});
+    });
+  });
