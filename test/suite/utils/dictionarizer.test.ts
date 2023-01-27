b45ca476fbe2403bd4dc15831ff71f01f90406e9
Kuma961 <all96p@libero.it>
2023-01-27 15:43:26 +0100

    Test parser refactoring

--- test/suite/utils/dictionarizer.test.ts
+++ test/suite/utils/dictionarizer.test.ts
@@ -1,8 +1,8 @@
 import { dictionarizer } from '../../../src/utils/dictionarizer';
 import * as assert from 'assert';
 
-describe('Dictionarizer Test Suite', () => {
-	it('dictionarizer must construct the correct data structure', () => {
+suite('Dictionarizer Test Suite', () => {
+	test('dictionarizer must construct the correct data structure', () => {
 		assert.deepStrictEqual(dictionarizer("constants.json"), 
 		{
 			"language-constants": ["U_INT", "UT_INT", "UR_INT", "T_INT", "R_INT", "CONST", "Q_CONST"]
