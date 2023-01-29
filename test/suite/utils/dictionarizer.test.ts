8c47ead2c6379edd3e4af903c1e68061ea8338b9
Kuma961 <all96p@libero.it>
2023-01-29 11:05:42 +0100

    aggiunta supporto diversi sistemi operativi

--- test/suite/utils/dictionarizer.test.ts
+++ test/suite/utils/dictionarizer.test.ts
@@ -1,4 +1,5 @@
 import 'mocha';
+import * as os from 'os';
 import * as assert from 'assert';
 import { dictionarizer } from '../../../src/utils/dictionarizer';
 import { PATH_TO_JSON_DICTIONARY } from '../../../src/utils/consts';
@@ -7,10 +8,18 @@ import { PATH_TO_JSON_DICTIONARY } from '../../../src/utils/consts';
 suite('Dictionarizer Test Suite', () => {
 	test('dictionarizer must construct the correct data structure', () => {
 
-
-		assert.deepStrictEqual(dictionarizer("../../" + PATH_TO_JSON_DICTIONARY.CONSTANTS),
-			{
-				'language-constants': ['U_INT', 'UT_INT', 'UR_INT', 'T_INT', 'R_INT', 'CONST', 'Q_CONST']
-			});
+		const operative_system = os.type();
+		if (operative_system === 'Linux' || operative_system === 'Darwin') {
+			assert.deepStrictEqual(dictionarizer(PATH_TO_JSON_DICTIONARY.CONSTANTS),
+				{
+					'language-constants': ['U_INT', 'UT_INT', 'UR_INT', 'T_INT', 'R_INT', 'CONST', 'Q_CONST']
+				});
+		}
+		else if (operative_system === 'Windows_NT') {
+			assert.deepStrictEqual(dictionarizer("../../" + PATH_TO_JSON_DICTIONARY.CONSTANTS),
+				{
+					'language-constants': ['U_INT', 'UT_INT', 'UR_INT', 'T_INT', 'R_INT', 'CONST', 'Q_CONST']
+				});
+		}
 	});
 });
